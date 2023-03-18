eb0993badee41aab7b81cc34d9618ac0486b6cda
Zeeshan Nawaz <zeeshanm.nawaz786@gmail.com>
2023-03-18 16:17:56 +0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,165 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { DragDropContext, Draggable, Droppable } from "react-beautiful-dnd";
 
-function App() {
+const reorderColumnList = (sourceCol, startIndex, endIndex) => {
+  const newTaskIds = Array.from(sourceCol.taskIds);
+  const [removed] = newTaskIds.splice(startIndex, 1);
+  newTaskIds.splice(endIndex, 0, removed);
+
+  const newColumn = {
+    ...sourceCol,
+    taskIds: newTaskIds,
+  };
+
+  return newColumn;
+};
+
+export default function Home() {
+  const [state, setState] = useState(initialData);
+
+  const onDragEnd = (result) => {
+    const { destination, source } = result;
+
+    // If user tries to drop in an unknown destination
+    if (!destination) return;
+
+    // if the user drags and drops back in the same position
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return;
+    }
+
+    // If the user drops within the same column but in a different positoin
+    const sourceCol = state.columns[source.droppableId];
+    const destinationCol = state.columns[destination.droppableId];
+
+    if (sourceCol.id === destinationCol.id) {
+      const newColumn = reorderColumnList(
+        sourceCol,
+        source.index,
+        destination.index
+      );
+
+      const newState = {
+        ...state,
+        columns: {
+          ...state.columns,
+          [newColumn.id]: newColumn,
+        },
+      };
+      setState(newState);
+      return;
+    }
+
+    // If the user moves from one column to another
+    const startTaskIds = Array.from(sourceCol.taskIds);
+    const [removed] = startTaskIds.splice(source.index, 1);
+    const newStartCol = {
+      ...sourceCol,
+      taskIds: startTaskIds,
+    };
+
+    const endTaskIds = Array.from(destinationCol.taskIds);
+    endTaskIds.splice(destination.index, 0, removed);
+    const newEndCol = {
+      ...destinationCol,
+      taskIds: endTaskIds,
+    };
+
+    const newState = {
+      ...state,
+      columns: {
+        ...state.columns,
+        [newStartCol.id]: newStartCol,
+        [newEndCol.id]: newEndCol,
+      },
+    };
+
+    setState(newState);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DragDropContext onDragEnd={onDragEnd}>
+      <div className="flex flex-col w-screen  h-screen bg-black text-white py-20 border border-black">
+        <div className="px-16 flex border-white">
+          {state.columnOrder.map((columnId) => {
+            const column = state.columns[columnId];
+            const tasks = column.taskIds.map((taskId) => state.tasks[taskId]);
+
+            // return <Column key={column.id} column={column} tasks={tasks} />;
+            // --------------------
+            return (
+              <div className="flex flex-col border w-96 h-96">
+                <h2 className="w-full border-white bg-red-900">
+                  {column.title}
+                </h2>
+
+                <Droppable droppableId={column.id}>
+                  {(droppableProvided, droppableSnapshot) => (
+                    <div
+                      className="border border-black"
+                      ref={droppableProvided.innerRef}
+                      {...droppableProvided.droppableProps}
+                    >
+                      {tasks.map((task, index) => (
+                        <Draggable
+                          key={task.id}
+                          draggableId={`${task.id}`}
+                          index={index}
+                        >
+                          {(draggableProvided, draggableSnapshot) => (
+                            <div
+                              className=" border bg-black w-full rounded-lg"
+                              ref={draggableProvided.innerRef}
+                              {...draggableProvided.draggableProps}
+                              {...draggableProvided.dragHandleProps}
+                            >
+                              <p>{task.content}</p>
+                            </div>
+                          )}
+                        </Draggable>
+                      ))}
+                      {droppableProvided.placeholder}
+                    </div>
+                  )}
+                </Droppable>
+              </div>
+            );
+            // --------------------
+          })}
+        </div>
+      </div>
+    </DragDropContext>
   );
 }
-
-export default App;
+const initialData = {
+  tasks: {
+    1: { id: 1, content: "Configure Next.js application" },
+    2: { id: 2, content: "Configure Next.js and tailwind " },
+    3: { id: 3, content: "Create sidebar navigation menu" },
+    4: { id: 4, content: "Create page footer" },
+    5: { id: 5, content: "Create page navigation menu" },
+    6: { id: 6, content: "Create page layout" },
+  },
+  columns: {
+    "column-1": {
+      id: "column-1",
+      title: "TO-DO",
+      taskIds: [1, 2, 3, 4, 5, 6],
+    },
+    "column-2": {
+      id: "column-2",
+      title: "IN-PROGRESS",
+      taskIds: [],
+    },
+    "column-3": {
+      id: "column-3",
+      title: "COMPLETED",
+      taskIds: [],
+    },
+  },
+  // Facilitate reordering of the columns
+  columnOrder: ["column-1", "column-2", "column-3"],
+};
